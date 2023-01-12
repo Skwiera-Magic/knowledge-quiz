85343bb2e5bb9978a0d2d9ce3a3f60331696ae3f
Skwiera <mckskwiera@gmail.com>
2023-01-12 12:23:56 +0000

    js files and first array of questions created

---
+++ assets/js/logic.js
@@ -0,0 +1,7 @@
+let questions = [
+    {question1: "#", answers: ["1", "2", "3", "4"], correctAnswer: 2},
+    {question2: "#", answers: ["1", "2", "3", "4"], correctAnswer: 2},
+    {question3: "#", answers: ["1", "2", "3", "4"], correctAnswer: 2},
+    {question4: "#", answers: ["1", "2", "3", "4"], correctAnswer: 2},
+];
+
