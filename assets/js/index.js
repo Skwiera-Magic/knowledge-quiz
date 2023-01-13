ef05c8e4f1034c9048c0540185e442aee087c4bd
Skwiera <mckskwiera@gmail.com>
2023-01-13 13:26:16 +0000

    added if and for elements, I'm able to go through all the screens now

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,41 @@
+let log = console.log.bind(document)
 let quizBox = document.querySelector(".wrapper");
 let startScreen = document.getElementById("start-screen")
+let answers = document.getElementById("choices")
+let endScreen = document.getElementById("end-screen")
 let questionsScreen = document.getElementById("questions")
 let questionTitle = document.getElementById("question-title")
+let questionList = document.createElement("ol")
 var questionNumber = 0;
 let questions = [
-    {question1: "who", answers: ["1", "2", "3", "4"], correctAnswer: 2},
-    {question2: "why", answers: ["1", "2", "3", "4"], correctAnswer: 2},
-    {question3: "where", answers: ["1", "2", "3", "4"], correctAnswer: 2},
-    {question4: "when", answers: ["1", "2", "3", "4"], correctAnswer: 2},
+    {question: "who", answers: ["1", "2", "3", "4"], correctAnswer: 2},
+    {question: "why", answers: ["1", "2", "3", "4"], correctAnswer: 2},
+    {question: "where", answers: ["1", "2", "3", "4"], correctAnswer: 2},
+    {question: "when", answers: ["1", "2", "3", "4"], correctAnswer: 2},
 ];
-quizBox.addEventListener("click", function (event) {
-    let element = event.target;
+// event listener checking if 'start' button was clicked
+quizBox.addEventListener("click", function (quizStart) {
+    let element = quizStart.target;
     if (element.matches("#start")) {
+        //changing classes so that start screen hides and questions show up
         startScreen.setAttribute("class", "hide")
         questionsScreen.setAttribute("class", "start")
+        //for loop to go through all the questions
+        for (let i = 0; i < questions.length; i++) {
+            questionTitle.textContent = questions[questionNumber].question
+            answers.textContent = questions[questionNumber].answers
+            log(questions[questionNumber].question)
+            log(questionNumber)
+            questionTitle.addEventListener("click", function(nextQuestion) {
+                let answerSelected = nextQuestion.target;
+                if (answerSelected.matches("#question-title"))
+                questionsScreen.setAttribute("class", "hide")
+                endScreen.setAttribute("class", "start")
+            questionNumber++;
+            log(questionNumber)
+            })
+        }
     }
-
 })
 
 
